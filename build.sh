487271a391c6572e7f258c0790c2188da01e81ec
Hồng Phát <hongphat.js@gmail.com>
2021-07-18 18:27:09 +0700

    deploy over ftp script

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+# Build source code to installable zip file
 zip zalopay.zip zalopay -r -x '*.git*'
+
+# Update new code over FTP
+. ./.env.sh
+lftp -e "set ftp:ssl-allow no; mirror -R zalopay public_html/modules/zalopay; quit" -u $FTP_USER,$FTP_PASSWD $FTP_HOST
+echo Deployed successfully to $FTP_HOST
